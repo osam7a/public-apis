c445ce3805a885e002e0dfb5555c61a86645e0fa
osam7a <90650351+osam7a@users.noreply.github.com>
2022-03-20 01:42:05 +0400

    added status code handling, bitly is done but not tested (need someone to test it (it need authorization))

--- public_apis/Categories/URL_shorteners/bitly.py
+++ public_apis/Categories/URL_shorteners/bitly.py
@@ -1,6 +1,12 @@
 import requests
 import aiohttp
 
+class Response:
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+        
+
 class Bitly:
     def __init__(self, auth):
         self.auth = auth
@@ -21,6 +27,41 @@ class Bitly:
             raise ValueError("Invalid Auth Token")
         elif resp.status_code == 429:
             raise TimeoutError("Too Many Requests")
+        elif resp.status_code == 200 or resp.status_code == 201:
+            _json = resp.json()
+            deeplinks = []
+            for i in _json['deeplinks']:
+                deeplinks.append(Response(
+                    guid=i['guid'],
+                    bitlink = i['bitlink']
+                    app_uri_path = i['app_uri_path'],
+                    install_url = i['install_url'],
+                    app_guid = i['app_guid'],
+                    os = i['os'],
+                    install_type = i['install_type']
+                    created = i['created'],
+                    modified = i['modified'],
+                    brand_guid = i['brand_guid']
+                ))
+            return Response(
+                references = _json['references'],
+                link = _json['link'],
+                id = _json['id'],
+                long_url = json['long_url'],
+                archived = json['archived'],
+                created_at = json['created_at'],
+                custom_bitlinks = _json['custom_bitlinks'],
+                tags = _json['tags']
+                deeplinks = deeplinks
+            )
+        elif resp.status_code in [400, 422, 429, 417, 503, 500]:
+            _json = resp.json()
+            return Response(
+                message = _json['message'],
+                description = _json['description'],
+                resource = _json['resource'],
+                errors = [Response(i['field'], i['error_code'], i['message']) for i in _json['errors']]
+            )
 
     async def shortenURL(self, long, groupguid, domain = None):
         data = {
@@ -38,5 +79,37 @@ class Bitly:
                     raise ValueError("Invalid Auth Token")
                 elif resp.status_code == 429:
                     raise TimeoutError("Too Many Requests")
+                elif resp.status_code == 200 or resp.status_code == 201:
+                    _json = await resp.json()
+                    return Response(
+                        references = _json['references'],
+                        link = _json['link'],
+                        id = _json['id'],
+                        long_url = json['long_url'],
+                        archived = json['archived'],
+                        created_at = json['created_at'],
+                        custom_bitlinks = _json['custom_bitlinks'],
+                        tags = _json['tags']
+                        deeplinks = [Response(
+                            guid=i['guid'],
+                            bitlink = i['bitlink']
+                            app_uri_path = i['app_uri_path'],
+                            install_url = i['install_url'],
+                            app_guid = i['app_guid'],
+                            os = i['os'],
+                            install_type = i['install_type']
+                            created = i['created'],
+                            modified = i['modified'],
+                            brand_guid = i['brand_guid']
+                        ) for i in _json['deeplinks']]
+                    )
+                elif resp.status_code in [400, 422, 429, 417, 503, 500]:
+                    _json = resp.json()
+                    return Response(
+                        message = _json['message'],
+                        description = _json['description'],
+                        resource = _json['resource'],
+                        errors = [Response(i['field'], i['error_code'], i['message']) for i in _json['errors']]
+                    )
 
     
